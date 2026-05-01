7a78746d18f2f4c1db3dc045414315a6237a9ddf
Carlos Oliveira <carlos.oliveira.dev.junior@gmail.com>
2026-05-01 15:27:21 -0300

    rotas get para busca de clientes implementadas

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,43 @@
 const express = require('express')
 const app = express()
+app.use(express.json());
+const pgp = require('pg-promise')();
+const connection = 'postgres://postgres:secret@postgres:5432/cadastro-clientes-db?sslmode=disable'
+const db = pgp(connection);
+
 const port = 4000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Rotas CRUD para Clientes
+app.get('/clientes', (req, res) => {
+  db.any('SELECT * FROM users')
+    .then(function(data) {
+      // success;
+      res.send(data);
+    })
+    .catch(function(error) {
+      // error;
+      res.send(error)
+    });
+});
+
+app.get('/clientes/:id', (req, res) => {
+  const { id } = req.params;
+  db.any('SELECT * FROM users WHERE id = $1', [id])
+    .then(function(data) {
+      // success;
+      res.send(data);
+    })
+    .catch(function(error) {
+      // error;
+      res.send(error)
+    });
+});
+
+app.post('/clientes', (req, res) => {});
+
+app.put('/clientes/:id', (req, res) => {});
+
+app.delete('/clientes/:id', (req, res) => {});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
