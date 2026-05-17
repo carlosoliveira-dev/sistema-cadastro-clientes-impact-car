97f40b15ddf0f3f240504b2109e50fc1058a069d
Carlos Oliveira <carlos.oliveira.dev.junior@gmail.com>
2026-05-17 08:16:23 -0300

    banco de dados configurado com os campos corretos

--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -2,10 +2,9 @@ CREATE TABLE IF NOT EXISTS users (
  id SERIAL PRIMARY KEY,
  name TEXT NOT NULL,
  email TEXT NOT NULL,
- password TEXT NOT NULL
+ telefone TEXT NOT NULL,
+ endereco TEXT NOT NULL
 );
 
-INSERT INTO users (name, email, password) VALUES
- ('John', 'john@gmail.com', 'p@ssword'),
- ('Maria', 'maria@gmail.com', 'p@ssword'),
- ('Camila', 'camila@gmail.com', 'p@ssword');
+INSERT INTO users (name, email, telefone, endereco) VALUES
+ ('John', 'john@gmail.com', '51900001111', 'Rua do John, 007');
